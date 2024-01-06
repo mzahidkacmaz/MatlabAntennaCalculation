1daab619b3924e566c195194d0ca780901a3a664
mzahidkacmaz <116164102+mzahidkacmaz@users.noreply.github.com>
2024-01-06 11:20:42 +0300

    Add files via upload

---
+++ AntennaCalculation.m
@@ -0,0 +1,17 @@
+% Anten parameter
+frequency = 433e6; % 433 MHz frequency
+speedOfLight = physconst('LightSpeed'); % speed of light 
+
+% Anten geometrisinin tanımlanması 
+wavelength = speedOfLight / frequency; % Wavelength calculation
+antennaLength = wavelength / 2; % Half wavelength antenna
+
+% Creation of antenna geometry
+antenna = design(dipole, frequency); % Dipole antenna design
+
+% Calculation of radiation pattern
+pattern(antenna, frequency); % Visualization of the antenna's radiation pattern
+
+% Display of antenna parameters
+disp('Anten Uzunluğu (m):');
+disp(antennaLength);
